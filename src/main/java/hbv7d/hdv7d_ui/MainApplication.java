f1aa4e0cfba1d9e59516a84462bbe3591c09021e
danielagustbjornsson <143106051+danielagust@users.noreply.github.com>
2025-04-06 23:31:04 +0000

    renamed to mainapplication

--- src/main/java/hbv7d/hdv7d_ui/MainApplication.java
+++ src/main/java/hbv7d/hdv7d_ui/MainApplication.java
@@ -2,22 +2,19 @@ package hbv7d.hdv7d_ui;
 
 //import HBV401G.Group.DayTour7.main.Api;
 //import hbv7d.Api;
-import hbv7d.Api;
+import hbv7d.api.Api;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import hbv7d.main.Main;
-
-
 
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         Api api = new Api();
