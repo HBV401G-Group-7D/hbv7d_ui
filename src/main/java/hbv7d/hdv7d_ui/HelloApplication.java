670624c8379f3ae5adbcb4b98ff6112128013d25
danielagustbjornsson <143106051+danielagust@users.noreply.github.com>
2025-04-06 22:35:45 +0000

    ui tenging við model

--- src/main/java/hbv7d/hdv7d_ui/HelloApplication.java
+++ src/main/java/hbv7d/hdv7d_ui/HelloApplication.java
@@ -1,10 +1,14 @@
 package hbv7d.hdv7d_ui;
 
+//import HBV401G.Group.DayTour7.main.Api;
+//import hbv7d.Api;
+import hbv7d.Api;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import hbv7d.*;
+import hbv7d.main.Main;
+
 
 
 
@@ -16,6 +20,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        Api api = new Api();
+        System.out.println(api.Name());
         stage.setScene(scene);
         stage.show();
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module hbv7d.hdv7d_ui {
     requires javafx.controls;
     requires javafx.fxml;
+//    requires hbv7d;
+    requires Group.DayTour7.main;
 
 
     opens hbv7d.hdv7d_ui to javafx.fxml;
