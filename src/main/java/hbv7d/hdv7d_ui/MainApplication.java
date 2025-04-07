cf8e87c9f7a7a36e6d810590c4a8641b8b6dc466
danielagustbjornsson <143106051+danielagust@users.noreply.github.com>
2025-04-07 11:32:04 +0000

    added a way to change the size of the scene

--- src/main/java/hbv7d/hdv7d_ui/MainApplication.java
+++ src/main/java/hbv7d/hdv7d_ui/MainApplication.java
@@ -3,6 +3,7 @@ package hbv7d.hdv7d_ui;
 //import HBV401G.Group.DayTour7.main.Api;
 //import hbv7d.Api;
 import hbv7d.api.Api;
+import hbv7d.hdv7d_ui.view.ViewSwitcher;
 import hbv7d.main.Main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,7 +18,8 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        Main main = new Main();
+        ViewSwitcher.setScene(scene);
+        ViewSwitcher.setStage(stage);
         stage.setTitle("Day Tour");
         stage.setScene(scene);
         stage.show();
--- src/main/java/hbv7d/hdv7d_ui/view/ViewSwitcher.java
+++ src/main/java/hbv7d/hdv7d_ui/view/ViewSwitcher.java
@@ -4,6 +4,7 @@ import hbv7d.hdv7d_ui.MainApplication;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -19,11 +20,64 @@ public class ViewSwitcher {
 
     private static final Map<View, Parent> cache = new HashMap<>();
 
-    private static Scene scene;
+    private static Scene scene; // the scene the view is on
+
+    private static Stage stage; // the stage the scene is on
 
     public static void setScene(Scene scene) {
         ViewSwitcher.scene = scene;
     }
+
+    public static Scene getScene() {
+        return scene;
+    }
+
+    public static void setStage(Stage stage) {
+        ViewSwitcher.stage = stage;
+    }
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    /**
+     * set the height of the scene
+     * @param height the height of the scene
+     */
+    public static void setSceneHeight(double height){
+        scene.getWindow().setHeight(height);
+    }
+
+    /**
+     * sets the width of the scene
+     * @param width the width of the scene
+     */
+    public static void setSceneWidth(double width){
+        scene.getWindow().setWidth(width);
+    }
+
+    /**
+     * sets both the width of the scene
+     * @param width the width of the scene
+     * @param height the height of the scene
+     */
+    public static void setSceneSize(double width, double height){
+        ViewSwitcher.setSceneWidth(width);
+        ViewSwitcher.setSceneHeight(height);
+    }
+
+    /**
+     * same as the switch to function but also sets the size of the scene
+     * @param view the view you want to choose from the enum in the view file
+     * @param isCache when you return to the view is it the same or does it get reset
+     * @param width the width of the scene
+     * @param height the height of the scene
+     */
+    public static void switchTo_WithSize(View view, boolean isCache, double width, double height){
+        ViewSwitcher.setSceneSize(width, height);
+        ViewSwitcher.switchTo(view, isCache);
+    }
+
 
     /**
      * this method is used to switch between Scenes
