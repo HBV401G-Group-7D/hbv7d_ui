e307dc677392b38139e4b59fb6e389c85fb320cd
danielagust <dab47@hi.is>
2025-04-08 15:22:36 +0000

    made the project use the main fxml

--- src/main/java/hbv7d/hdv7d_ui/MainApplication.java
+++ src/main/java/hbv7d/hdv7d_ui/MainApplication.java
@@ -16,8 +16,8 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
         ViewSwitcher.setScene(scene);
         ViewSwitcher.setStage(stage);
         stage.setTitle("Day Tour");
