05ca19559ab8a41c5320c9f516a575e57523e587
danielagust <dab47@hi.is>
2025-04-08 18:08:06 +0000

    added a lot of ids and actions

--- src/main/java/hbv7d/hdv7d_ui/controller/MainController.java
+++ src/main/java/hbv7d/hdv7d_ui/controller/MainController.java
@@ -1,6 +1,40 @@
 package hbv7d.hdv7d_ui.controller;
 
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class MainController {
 
+
+    public Button confirmBookingButton;
+    public Button userPageButton;
+    public Button companyPageButton;
+    public Button resetFilters;
+
+    public Button byPrice;
+    public Button byGroupSize;
+    public Button byLocation;
+    public Button byType;
+    public Button byDuration;
+
+    public TableColumn date;
+    public TableColumn price;
+    public TableColumn group_size;
+    public TableColumn seatsTaken;
+    public TableColumn location;
+    public TableColumn pickUp;
+    public TableColumn type;
+    public TableColumn difficulty;
+    public TableColumn text;
+
+    public void onConfirmBooking(ActionEvent actionEvent) {
+    }
+
+    public void onUserPage(ActionEvent actionEvent) {
+    }
+
+    public void onCompanyPage(ActionEvent actionEvent) {
+    }
 }
