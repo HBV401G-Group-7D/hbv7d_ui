93a8b2e2e1cb70f6972a8d9eeee0641c0f93ff45
danielagust <dab47@hi.is>
2025-04-08 18:28:23 +0000

    added default view switch
    note make sure to change the size of the scene to match the correct size for the scene

--- src/main/java/hbv7d/hdv7d_ui/controller/MainController.java
+++ src/main/java/hbv7d/hdv7d_ui/controller/MainController.java
@@ -1,6 +1,8 @@
 package hbv7d.hdv7d_ui.controller;
 
 
+import hbv7d.hdv7d_ui.view.View;
+import hbv7d.hdv7d_ui.view.ViewSwitcher;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -19,22 +21,24 @@ public class MainController {
     public Button byType;
     public Button byDuration;
 
-    public TableColumn date;
-    public TableColumn price;
-    public TableColumn group_size;
-    public TableColumn seatsTaken;
-    public TableColumn location;
-    public TableColumn pickUp;
-    public TableColumn type;
-    public TableColumn difficulty;
-    public TableColumn text;
+//    public TableColumn date;
+//    public TableColumn price;
+//    public TableColumn group_size;
+//    public TableColumn seatsTaken;
+//    public TableColumn location;
+//    public TableColumn pickUp;
+//    public TableColumn type;
+//    public TableColumn difficulty;
+//    public TableColumn text;
 
     public void onConfirmBooking(ActionEvent actionEvent) {
     }
 
     public void onUserPage(ActionEvent actionEvent) {
+        ViewSwitcher.switchTo_WithSize(View.USER, false, 600, 400);
     }
 
     public void onCompanyPage(ActionEvent actionEvent) {
+        ViewSwitcher.switchTo_WithSize(View.COMPANY, false, 600, 400);
     }
 }
